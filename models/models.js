0d829517be8a65deb381324476396fcd754993ff
Natalia K <WhiteKNS@users.noreply.github.com>
2016-04-06 01:38:00 +0300

    Update models.js

--- models/models.js
+++ models/models.js
@@ -1,12 +1,10 @@
 module.exports = function(connection) {
 
     var User = require('./user')(connection);
-    var Person = require('./person')(connection);
     var Thing = require('./thing')(connection);
 
     return {
         user: User,
-        person: Person,
         thing: Thing
     }
 }
